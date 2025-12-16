9808cd38e04996a7254e8c0298d37c0e54f3d9a0
Geethika <april.appy24@gmail.com>
2025-12-16 22:56:46 +0530

    Database updated to MySQL

--- Project1/settings.py
+++ Project1/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'Project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'firstdb',
+        'HOST' : 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : 'REMOVED_SECRET',
     }
 }
 
