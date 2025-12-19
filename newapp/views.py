c7a5c5798d20d7142fb8e1ac1a4f7acdd3b234e3
Geethika <april.appy24@gmail.com>
2025-12-20 03:17:58 +0530

    rendered unordered list

--- newapp/views.py
+++ newapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 def say_hello(request):
-    x = 1
-    y = 2
-    return render(request,'index.html',{'name' : 'lemon'})
+    #query_set = Product.objects.all()
+
+    #Iterating over the query_set to generate SQL code to send to the database
+    # for product in query_set:
+    #     print(product)
+
+    #get() method
+    # product = Product.objects.get(pk=1)
+
+    queryset = Product.objects.filter(unit_price__range = (20,30))
+
+    return render(request,'index.html',{'name' : 'lemon','products': list(queryset)})
